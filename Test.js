7e54e912789c9d407d2fce94cf9c86570e8e10d0
SamsonSun <gsamsonsun@gmail.com>
2022-10-16 17:24:09 +0800

    package class made

--- Test.js
+++ Test.js
@@ -1,7 +1,7 @@
-// require('dotenv').config({
-//     path: './.env'
-// });
-
+require('dotenv').config({
+    path: './.env'
+});
+const ntdb = require('./src/Notion_database');
 const client = require('./index');
 
 const ID = process.env;
@@ -10,14 +10,18 @@ const token = ID.NOTION_TOKEN;
 
 function run() {
 
-    const nt = client.init(token);
+    var db_test = new client.init(token,ndb_id);
+    console.log(db_test.query(
+        {page_size:2}
+    ));
     
-    nt.databases.query({
-        database_id: ndb_id,
-        page_size: 2
-    }).then(data => {
-        console.log(data.results[0].properties);
-    })
+    // nt.databases.query({
+    //     database_id: ndb_id,
+    //     page_size: 2
+    // }).then(data => {
+    //     ntdb.database(data);
+    //     console.log(data.results[0].properties);
+    // })
 
 
 }
--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-const notionPort = {
-  init : require('./src/Notion_init.js'),
+const ntdb = require('./src/Notion_database')
+const {
+  Client
+} = require('@notionhq/client')
+
+
+class s_nt_db_init{
+  constructor(secret,ntdb_id){
+    this.secret_key = secret;
+    this.ntdb_id = ntdb_id;
+    this.client = new Client({
+      auth: secret,
+    });
+  }
+  query(parameter){
+    this.parameter = parameter;
+    this.parameter.database_id =  this.ntdb_id;
+
+    return ntdb.database(this.client.databse.query(this.parameter));
+  }
 }
+
+
+const notionPort = {
+  init : s_nt_db_init,
+}
+
+// require('dotenv').config();
+
 
 module.exports = notionPort;
